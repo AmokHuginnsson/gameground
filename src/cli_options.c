ab4fb4615aa38e18568f98afa67895bd59066f6d
Marcin Konarski <marcin.konarski@codestation.org>
2005-08-08 16:18:28 +0000

    Initial revision
    git-svn-id: file:///home/amok/repo/galaxy/trunk@983 e5d723ff-ac09-4e1e-99b2-b35108d02165

---
+++ src/cli_options.c
@@ -0,0 +1,84 @@
+/*
+---             `galaxy' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski              ---
+
+	cli_options.c - this file is integral part of `galaxy' project.
+
+	i.  You may not make any changes in Copyright information.
+	ii. You must attach Copyright information to any part of every copy
+	    of this software.
+
+Copyright:
+
+ You are free to use this program as is, you can redistribute binary
+ package freely but:
+  1. You can not use any part of sources of this software.
+  2. You can not redistribute any part of sources of this software.
+  3. No reverse engineering is allowed.
+  4. If you want redistribute binary package you can not demand any fees
+     for this software.
+     You can not even demand cost of the carrier (CD for example).
+  5. You can not include it to any commercial enterprise (for example 
+     as a free add-on to payed software or payed newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+
+#include <stdhapi.h>
+
+#include "cli_options.h"
+#include "version.h"
+#include "setup.h"
+
+using namespace stdhapi;
+using namespace stdhapi::hcore;
+  
+/* Set all the option flags according to the switches specified.
+   Return the index of the first non-option argument.                    */
+
+void usage ( void ) __attribute__ ( ( __noreturn__ ) );
+void usage ( void )
+	{
+	printf ( "%s - "
+"does very much usefull things ... really\n", setup.f_pcProgramName );
+	printf ( "Usage: %s [OPTION]... [FILE]...\n", setup.f_pcProgramName );
+	printf (
+"Options:\n"
+"  -q, --quiet, --silent      inhibit usual output\n"
+"  --verbose                  print more information\n"
+"  -h, --help                 display this help and exit\n"
+"  -V, --version              output version information and exit\n" );
+	exit ( setup.f_bHelp ? 0 : 1 );
+	}
+
+void version ( void ) __attribute__ ( ( __noreturn__ ) );
+void version ( void )
+	{
+	printf ( "`galaxy' %s\n", VER );
+	exit ( 0 );
+	}
+
+int decode_switches ( int a_iArgc, char ** a_ppcArgv )
+	{
+	M_PROLOG
+	int l_iUnknown = 0, l_iNonOption = 0;
+	OOption l_psOptions [ ] =
+		{
+			{ "quiet",		'q', OOption::D_NONE,	D_BOOL,	& setup.f_bQuiet,		NULL },
+			{ "silent",		'q', OOption::D_NONE,	D_BOOL,	& setup.f_bQuiet,		NULL },
+			{ "verbose",	'v', OOption::D_NONE,	D_BOOL,	& setup.f_bVerbose,	NULL },
+			{ "help",			'h', OOption::D_NONE,	D_BOOL,	& setup.f_bHelp,		usage },
+			{ "version",	'V', OOption::D_NONE,	D_NONE,	NULL,								version }
+		};
+	l_iNonOption = cl_switch::decode_switches ( a_iArgc, a_ppcArgv, l_psOptions,
+			sizeof ( l_psOptions ) / sizeof ( OOption ), & l_iUnknown );
+	if ( l_iUnknown > 0 )
+		usage ( );
+	return ( l_iNonOption );
+	M_EPILOG
+	}
+
---
+++ src/rc_options.c
@@ -0,0 +1,57 @@
+/*
+---       `galaxy' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski         ---
+
+	rc_options.c - this file is integral part of `galaxy' project.
+
+	i.  You may not make any changes in Copyright information.
+	ii. You must attach Copyright information to any part of every copy
+	    of this software.
+
+Copyright:
+
+ You are free to use this program as is, you can redistribute binary
+ package freely but:
+  1. You can not use any part of sources of this software.
+  2. You can not redistribute any part of sources of this software.
+  3. No reverse engineering is allowed.
+  4. If you want redistribute binary package you can not demand any fees
+     for this software.
+     You can not even demand cost of the carrier (CD for example).
+  5. You can not include it to any commercial enterprise (for example 
+     as a free add-on to payed software or payed newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <stdhapi.h>
+M_CVSID ( "$CVSHeader$" );
+
+#include "setup.h"
+
+using namespace stdhapi::hcore;
+
+bool set_variables ( HString & a_roOption, HString & a_roValue )
+	{
+	printf ( "option: [%s], value: [%s]\n", ( char * ) a_roOption,
+			( char * ) a_roValue );
+	return ( false );
+	}
+
+int process_galaxyrc_file ( void )
+	{
+	OVariable l_psVars [ ] =
+		{
+			{ D_HSTRING, "log_path", & setup.f_oLogPath },
+			{ D_NONE, NULL, NULL }
+		};
+	rc_file::process_rc_file ( "galaxy", NULL, l_psVars, NULL );
+	if ( ! setup.f_oLogPath )
+		setup.f_oLogPath = "galaxy.log";
+	return ( 0 );
+	}
+
