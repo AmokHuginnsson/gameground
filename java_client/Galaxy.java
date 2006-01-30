2618d0f8842cbce5e83d3cb1a7c1beefd0c7bb13
Marcin Konarski <marcin.konarski@codestation.org>
2006-01-30 00:36:24 +0000

    This java program can be started both as applet and standalone application.
    git-svn-id: file:///home/amok/repo/galaxy/trunk@1157 e5d723ff-ac09-4e1e-99b2-b35108d02165

--- java_client/Galaxy.java
+++ java_client/Galaxy.java
@@ -4,67 +4,82 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.net.URL;
 
-public class Galaxy extends JApplet implements ActionListener {
-	JPanel mainPanel;
-	JPanel up;
-	JPanel down;
-	JTextField edit0;
-	JTextField edit1;
+class HGUIMain extends JPanel implements ActionListener {
+	JPanel _mainPanel;
+	JPanel _up;
+	JPanel _down;
+	JTextField _edit0;
+	JTextField _edit1;
 	JButton go;
-	ButtonGroup radios;
-	JRadioButton radio0;
-	JRadioButton radio1;
-	JRadioButton radio2;
+	ButtonGroup _radios;
+	JRadioButton _radio0;
+	JRadioButton _radio1;
+	JRadioButton _radio2;
 	AudioClip clip;
-	public static final long serialVersionUID = 13l;
-	public void init() {
-		radio0 = new JRadioButton( "Wilk" );
-		radio1 = new JRadioButton( "Kruk" );
-		radio2 = new JRadioButton( "Nied¼wie¼" );
-		radios = new ButtonGroup();
-		edit0 = new JTextField( 20 );
-		edit1 = new JTextField( 20 );
+	public HGUIMain(String label) {
+		_radio0 = new JRadioButton( "Wilk" );
+		_radio1 = new JRadioButton( "Kruk" );
+		_radio2 = new JRadioButton( "Nied¼wie¼" );
+		_radios = new ButtonGroup();
+		_edit0 = new JTextField( 20 );
+		_edit1 = new JTextField( 20 );
 		go = new JButton( "Go !" );
-		mainPanel = new JPanel();
-		up = new JPanel();
-		down = new JPanel();
+		_mainPanel = new JPanel();
+		_up = new JPanel();
+		_down = new JPanel();
 		
-		radios.add( radio0 );
-		radios.add( radio1 );
-		radios.add( radio2 );
+		_radios.add( _radio0 );
+		_radios.add( _radio1 );
+		_radios.add( _radio2 );
 		
-		mainPanel.setLayout( new BoxLayout( mainPanel, BoxLayout.Y_AXIS ) );
-		mainPanel.add( up );
-		mainPanel.add( down );
-		up.add( radio0 );
-		up.add( radio1 );
-		up.add( radio2 );
-		up.add( edit0 );
-		up.add( edit1 );
-		down.add( go );
+		_mainPanel.setLayout( new BoxLayout( _mainPanel, BoxLayout.Y_AXIS ) );
+		_mainPanel.add( _up );
+		_mainPanel.add( _down );
+		_up.add( _radio0 );
+		_up.add( _radio1 );
+		_up.add( _radio2 );
+		_up.add( _edit0 );
+		_up.add( _edit1 );
+		_down.add( go );
 		
 		setSize( 400, 200 );
-		getContentPane().add( mainPanel );
-		up.setSize( 380, 100 );
-		up.doLayout();
+		add( _mainPanel );
+		_up.setSize( 380, 100 );
+		_up.doLayout();
 
 		go.addActionListener( this );
-		radio0.addActionListener( this );
-		radio1.addActionListener( this );
-		radio2.addActionListener( this );
+		_radio0.addActionListener( this );
+		_radio1.addActionListener( this );
+		_radio2.addActionListener( this );
 	}
-	public void actionPerformed( ActionEvent event ) {
-		Object source = event.getSource();
+	public void actionPerformed( ActionEvent $event ) {
+		Object source = $event.getSource();
 		if ( source == go ) {
-			System.out.println( "go" );
-			
-			edit1.setText( edit0.getText() );
+			System.out.println( "go" );		
+			_edit1.setText( _edit0.getText() );
 		} else if ( source.getClass().getName() == "javax.swing.JRadioButton" ) {
 			System.out.println( "radio" );
-			edit0.setText( ( ( JRadioButton ) source ).getText() );
+			_edit0.setText( ( ( JRadioButton ) source ).getText() );
 		} else {
 			System.out.println( source.getClass().getName() );
 		}
 	}
 }
+
+public class /* Application or applet name: */ Galaxy extends JApplet {
+	public static final long serialVersionUID = 13l;
+	private static JFrame _frame;
+
+	public void init() {
+		add( new HGUIMain ("Push Me") );
+	} 
+
+	static public void main (String argv[]) {
+		_frame = new JFrame();
+		_frame.add( new HGUIMain( "Push Me" ) );
+		_frame.pack();
+		_frame.setVisible( true );
+	}
+}
+
 
