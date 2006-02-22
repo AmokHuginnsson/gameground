bd6220c26d3094ca71815978e3ba79728a62100c
Marcin Konarski <marcin.konarski@codestation.org>
2006-02-22 17:09:48 +0000

    New class added, used to hold move information.
    git-svn-id: file:///home/amok/repo/galaxy/trunk@1216 e5d723ff-ac09-4e1e-99b2-b35108d02165

---
+++ java_client/HMove.java
@@ -0,0 +1,12 @@
+class HMove {
+//--------------------------------------------//
+	int _sourceSystem;
+	int _destinationSystem;
+	int _fleet;
+//--------------------------------------------//
+	public HMove() {
+		_sourceSystem = -1;
+		_destinationSystem = -1;
+		_fleet = -1;
+	}
+}
