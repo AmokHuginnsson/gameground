8ea39b2266c54ff88152b349f5a2c69d22bb732f
Marcin Konarski <marcin.konarski@codestation.org>
2007-10-27 13:00:10 +0000

    Added missing file.
    git-svn-id: file:///home/amok/repo/gameground/trunk@2342 e5d723ff-ac09-4e1e-99b2-b35108d02165

---
+++ java_client/HLogicInfo.java
@@ -0,0 +1,9 @@
+public class HLogicInfo {
+	public String _symbol;
+	public String _name;
+	public String _defaults;
+	public HLogicInfo( String $symbol, String $name ) {
+		_symbol = $symbol;
+		_name = $name;
+	}
+}
