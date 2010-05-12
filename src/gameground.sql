673d237712debc89448fc0a6c324bd9ee531a381
Marcin Konarski <marcin.konarski@codestation.org>
2010-05-12 18:36:18 +0200

    Users are automatically added now.

--- src/gameground.sql
+++ src/gameground.sql
@@ -178,7 +178,3 @@ INSERT INTO tbl_game ( name ) VALUES ( 'boggle' );
 INSERT INTO tbl_game ( name ) VALUES ( 'galaxy' );
 INSERT INTO tbl_game ( name ) VALUES ( 'gomoku' );
 
-INSERT INTO v_user_session ( login, password, name, email, registered ) VALUES ( 'amok', '1c29cf0ceb89afce131e27b76c18af1e9cf7f5e3', 'Marcin Konarski', 'amok13@o2.pl', datetime( 'now', 'localtime' ) );
-INSERT INTO v_user_session ( login, password, name, email ) VALUES ( 'kamawanai', '0c7ebd714bbcfca66cc23ce5652569ccb48d6e48', 'Diana Blaszczyk', 'diana.blaszczyk@gmail.com' );
-INSERT INTO v_user_session ( login, password ) VALUES ( 'aNeutrino', 'b4434d5d3638c82cf2dc25c9e4cb75b1f7110e05' );
-
