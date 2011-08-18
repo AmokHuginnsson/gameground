c616c140590bae6f881a9a459d198e8ec10056bc
Marcin Konarski <marcin.konarski@codestation.org>
2011-08-18 21:10:11 +0200

    Fixed sound player fixed.

--- java_client/Sound.java
+++ java_client/Sound.java
@@ -2,7 +2,20 @@ import java.io.InputStream;
 import java.io.BufferedInputStream;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineListener;
+import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.Clip;
+
+class ClipCloser implements LineListener {
+	Clip _clip = null;
+	ClipCloser( Clip $clip ) {
+		_clip = $clip;
+	}
+	public void update( LineEvent event ) {
+		if ( event.getType() == LineEvent.Type.STOP )
+			_clip.close();
+	}
+}
 
 class Sound {
 	static public void play( String $name ) {
@@ -12,6 +25,9 @@ class Sound {
 			BufferedInputStream bufferedInputStream = new BufferedInputStream( is );
 			AudioInputStream audioStream = AudioSystem.getAudioInputStream( bufferedInputStream );
 			clip.open( audioStream );
+			clip.setFramePosition( 0 );
+			clip.loop( 0 );
+			clip.addLineListener( new ClipCloser( clip ) );
 			clip.start(); 
 		} catch ( javax.sound.sampled.LineUnavailableException lue ) {
 			Con.err( "LineUnavailableException: " + lue.getMessage() );
