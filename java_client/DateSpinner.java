9e09f5c30a9b23c97e321c8cb237c0a249105097
Marcin Konarski <marcin.konarski@codestation.org>
2012-10-29 14:38:31 +0100

    DateSpinner works flawlessly.

--- java_client/DateSpinner.java
+++ java_client/DateSpinner.java
@@ -11,11 +11,16 @@ import javax.swing.text.InternationalFormatter;
 import javax.swing.text.DefaultCaret;
 import java.awt.event.FocusListener;
 import java.awt.event.FocusEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 import javax.swing.SwingUtilities;
 
-public class DateSpinner extends JSpinner implements FocusListener {
+public class DateSpinner extends JSpinner implements ChangeListener, FocusListener {
 	public static final long serialVersionUID = 17l;
 	JSpinner.DateEditor _editor = null;
+	JFormattedTextField _textfield = null;
+	private boolean _ignoreEvents = false;
+	int _selectedField = 1;
 	public DateSpinner( InitClass $init ) {
 		super( new SpinnerDateModel() );
 		String[] tokens = $init.data().split( "\\|", 2 );
@@ -26,41 +31,48 @@ public class DateSpinner extends JSpinner implements FocusListener {
 		dm.setValue( new Date( 0 ) );
 		dm.setEnd( new Date( max * 1000 ) );
 		dm.setCalendarField( Calendar.MINUTE );
-		JFormattedTextField tf = _editor.getTextField();
-		DefaultFormatter f = (DefaultFormatter)tf.getFormatter();
+		_textfield = _editor.getTextField();
+		DefaultFormatter f = (DefaultFormatter)_textfield.getFormatter();
 		DateFormat df = ((DateFormat)((InternationalFormatter)f).getFormat());
 		df.setLenient( false );
 		df.setTimeZone( new SimpleTimeZone( 0, "UTC" ) );
 		f.setCommitsOnValidEdit( true );
 		f.setAllowsInvalid( false );
 		f.setOverwriteMode( true );
-		tf.setHorizontalAlignment( JTextField.RIGHT );
+		_textfield.setHorizontalAlignment( JTextField.RIGHT );
 		setEditor( _editor );
-		tf.addFocusListener( this );
+		_textfield.addFocusListener( this );
+		addChangeListener( this );
 	}
-	@Override
-	public void focusGained( FocusEvent e ) {
-		final JFormattedTextField tf = (JFormattedTextField)e.getSource();
+	void selectField() {
 		SwingUtilities.invokeLater( new Runnable() {
 			@Override
 			public void run() {
-				JSpinner spinner = (JSpinner)tf.getParent().getParent();
-				tf.setCaretPosition( 3 );
-				tf.moveCaretPosition( 5 );
+				_textfield.setCaretPosition( _selectedField * 3 );
+				_textfield.moveCaretPosition( _selectedField * 3 + 2 );
 			}
 		} );
 	}
 	@Override
+	public void focusGained( FocusEvent e ) {
+		selectField();
+	}
+	@Override
 	public void focusLost( FocusEvent e ) {
+		_selectedField = _textfield.getCaretPosition() / 3;
 	}
 	@Override
 	public void setValue( Object $seconds ) {
+		_ignoreEvents = true;
 		if ( $seconds instanceof Date )
 			super.setValue( $seconds );
 		else {
 			int value = ((Integer)$seconds).intValue();
 			super.setValue( new Date( value * 1000 ) );
 		}
+		if ( _textfield != null )
+			selectField();
+		_ignoreEvents = false;
 	}
 	public int getValueInt() {
 		SpinnerDateModel dm = (SpinnerDateModel)getModel();
@@ -68,5 +80,9 @@ public class DateSpinner extends JSpinner implements FocusListener {
 		Calendar c = Calendar.getInstance();
 		c.setTime( d );
 		return ( (int)( c.getTimeInMillis() / 1000l ) );
+	}
+	public void stateChanged( ChangeEvent e ) {
+		if ( ( _textfield != null ) && ! _ignoreEvents )
+			_selectedField = _textfield.getCaretPosition() / 3;
 	}
 }
