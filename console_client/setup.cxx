413f42780dc52d98f8d36c46b557d45f113770bb
Marcin Konarski <marcin.konarski@codestation.org>
2014-05-13 17:21:10 +0200

    Sync to yaal.

--- console_client/setup.cxx
+++ console_client/setup.cxx
@@ -58,7 +58,7 @@ void OSetup::test_setup( void ) {
 		yaal::tools::util::failure( 1,
 				_( "quiet and verbose options are exclusive\n" ) );
 	if ( _verbose )
-		clog.reset( make_pointer<HFile>( stdout ) );
+		clog.reset( make_pointer<HFile>( stdout, false ) );
 	else
 		std::clog.rdbuf( NULL );
 	if ( _quiet ) {
